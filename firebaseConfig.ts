fed7df0bf74921e8650e55055e62044b61b8cea3
Varinder Sharma <varindersharma786786@gmail.com>
2025-01-25 14:56:56 +0530

    Reapply "final changes"
    This reverts commit e8d453b5b16bc0e87d0ff238d6b23a19e8755e43.

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,27 +1,24 @@
 import { initializeApp } from "firebase/app";
-import {
-  setPersistence,
-  getAuth,
-  ReactNativeAsyncStorage,
-} from "firebase/auth";
+import { getAuth, setPersistence,ReactNativeAsyncStorage } from "firebase/auth";
 import { getFirestore, collection } from "firebase/firestore";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBPr-mFenCuO1bcj5YAGjKWP2AHicZcyv4",
-  authDomain: "app-dev-2319a.firebaseapp.com",
-  projectId: "app-dev-2319a",
-  storageBucket: "app-dev-2319a.firebasestorage.app",
-  messagingSenderId: "904809183680",
-  appId: "1:904809183680:web:7b393ab4e111f291f87025",
+  // ... your Firebase configuration
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const persistence=ReactNativeAsyncStorage(AsyncStorage)
-setPersistence(auth,);
+const auth = getAuth(app);
 
-export const db = getFirestore(app);
-export const userRef = collection(db, "user");
-export const roomRef = collection(db, "room");
+// Use AsyncStorage for local persistence
+setPersistence(auth, { 
+  type:ReactNativeAsyncStorage, 
+
+  storage: AsyncStorage 
+}); 
+
+const db = getFirestore(app);
+const userRef = collection(db, "user");
+const roomRef = collection(db, "room");
+
+export { auth, db, userRef, roomRef };
