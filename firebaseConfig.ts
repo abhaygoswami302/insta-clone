a14b405ac080ac16da0e0dd8e934b624cae3f595
Varinder Sharma <varindersharma786786@gmail.com>
2025-01-25 15:05:52 +0530

    add local env file

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,26 +1,30 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, setPersistence,ReactNativeAsyncStorage } from "firebase/auth";
+import {
+  getAuth,
+  setPersistence,
+  ReactNativeAsyncStorage,
+} from "firebase/auth";
 import { getFirestore, collection } from "firebase/firestore";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBPr-mFenCuO1bcj5YAGjKWP2AHicZcyv4",
-  authDomain: "app-dev-2319a.firebaseapp.com",
-  projectId: "app-dev-2319a",
-  storageBucket: "app-dev-2319a.firebasestorage.app",
-  messagingSenderId: "904809183680",
-  appId: "1:904809183680:web:7b393ab4e111f291f87025",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
 // Use AsyncStorage for local persistence
-setPersistence(auth, { 
-  type:ReactNativeAsyncStorage, 
+setPersistence(auth, {
+  type: ReactNativeAsyncStorage,
 
-  storage: AsyncStorage 
-}); 
+  storage: AsyncStorage,
+});
 
 const db = getFirestore(app);
 const userRef = collection(db, "user");
