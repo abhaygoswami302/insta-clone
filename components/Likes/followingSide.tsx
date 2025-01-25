d730c2ee4e449ca287627c5276f6bddaca14674c
Varinder Sharma <varindersharma786786@gmail.com>
2025-01-25 15:12:53 +0530

    add following Componenet in Like page

---
+++ components/Likes/followingSide.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const FollowingSide = () => {
+  return (
+    <View>
+      <Text>FollowingSide</Text>
+    </View>
+  )
+}
+
+export default FollowingSide
